9d505c9854c1c335c2509dfa58d63b48855f05e3
Mani Kumar Reddy Kancharla <kmanikumarreddy@live.com>
2018-11-23 23:15:44 +0530

    adding test case to test suite

---
+++ Test Suites/Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e0ad2474-fe2d-4009-96a8-3e49169dc256</testSuiteGuid>
+   <testCaseLink>
+      <guid>a612949f-1587-4cef-893e-9a7deb582822</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WHole case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
